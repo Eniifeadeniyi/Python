3d181b829bc1a0c1c27f59858b23a25779400845
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 00:57:49 +0100

    Delete TwoTwo.py

--- TwoTwo.py
+++
@@ -1,5 +0,0 @@
-rating = input('Enter an integer rating between 1 and 10')
-
-#For the code to read integer into the variable, convert the type str to type int. Input always returns string.
-#CORRECTION:
-rating = int(input('Enter an integer rating between 1 and 10'))

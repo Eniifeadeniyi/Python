adfc6774b2ec75ecdb960603b023ceb1372fbf03
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 00:58:04 +0100

    Delete PetChooser.py

--- PetChooser.py
+++
@@ -1,8 +0,0 @@
-pet = input("Enter your favourite pet: ")
-#Ask for user to type in their favourite pet.
-
-if pet == "dog":
-	print("Woof! Dogs are awesome!")
-else:
-	print("Cool choice, but I love dogs!")
-#It should print Woof, Dogs are awesome!, if the user types in dog; and Cool choice, but I love dogs!, if not.

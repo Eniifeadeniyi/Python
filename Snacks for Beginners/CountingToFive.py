8dae0e6f5b4d88734eca5fb327634800f9a32792
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 13:15:10 +0100

    Update CountingToFive.py

--- Snacks for Beginners/CountingToFive.py
+++ Snacks for Beginners/CountingToFive.py
@@ -1,6 +1,8 @@
-count = 1
 #It should start counting from 1.
- 
-while count < 6:   #I want it to end at 5
-	print("Number:", count)
-	count = count + 1
+#Print numbers 1 to 5 
+
+print("Number:1", "Number:2", "Number:3", "Number:4", "Number:5")
+print("Number:1", "Number:2", "Number:3", "Number:4", "Number:5")
+print("Number:1", "Number:2", "Number:3", "Number:4", "Number:5")
+print("Number:1", "Number:2", "Number:3", "Number:4", "Number:5")
+print("Number:1", "Number:2", "Number:3", "Number:4", "Number:5")

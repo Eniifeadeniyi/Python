9d66db21d375b9eb08e1b2c5a591fc574fc454be
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 12:59:14 +0100

    Update TwoOne.py

--- Chapter 2/TwoOne.py
+++ Chapter 2/TwoOne.py
@@ -3,7 +3,8 @@
 x = 2
 y = 3
 
-print('x =', x)
-print('Value of', x, '+', x, 'is', (x + x))
-print('x =')
-print((x + y), '=', (y + x))
+print('x =', x) # 2
+print('Value of', x, '+', x, 'is', (x + x))  #Value of 2 + 2 is 4
+print('x =') # x =
+print((x + y), '=', (y + x))  #5 = 5
+

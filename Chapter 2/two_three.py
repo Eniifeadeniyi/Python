31f07469115283eb37e2eede156d56f7887f4af8
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 23:56:20 +0100

    Update two_three.py

--- Chapter 2/two_three.py
+++ Chapter 2/two_three.py
@@ -1,6 +1,7 @@
-grade = int(input("Enter your grade: "))   #Ask user to input score
-#convert to integer so it returns as integer instead of string and it can be compare with 90
+#Ask user to input score
+#Compare user's grade with 90
 
-if grade >= 90:                            #Compare user's grade with 90
+grade = int(input("Enter your grade: "))  0
+
+if grade >= 90:                            
 	print("Congratulations! Your grade of " + str(grade) + " earns you an A in this course.")
-#convert grade back to string so it can concatenate with string.

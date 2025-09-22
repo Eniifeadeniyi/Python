3f737335cd1947309939e440b6a633a5a22730b4
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 04:47:55 +0100

    Update TwoThirteen.py

--- Chapter 2/TwoThirteen.py
+++ Chapter 2/TwoThirteen.py
@@ -2,16 +2,16 @@
 	Trying to see how big Python integers can be.
 """
 
-Trial_A = 2 * 2
-Trial_B = 22 ** 2
-Trial_C = 222 ** 2
-Trial_D = 222 ** 2 ** 3
-Trial_E = 2 ** 22
-Trial_F = 2 ** 9 ** 3
+trial_A = 2 * 2
+trial_B = 22 ** 2
+trial_C = 222 ** 2
+trial_D = 222 ** 2 ** 3
+trial_E = 2 ** 22
+trial_F = 2 ** 9 ** 3
 
-print("Trial_A =", Trial_A)
-print("Trial_B =", Trial_B)
-print("Trial_C =", Trial_C)
-print("Trial_D =", Trial_D)
-print("Trial_E =", Trial_E)
-print("Trial_F =", Trial_F)
+print("trial_A =", trial_A)
+print("trial_B =", trial_B)
+print("trial_C =", trial_C)
+print("trial_D =", trial_D)
+print("trial_E =", trial_E)
+print("trial_F =", trial_F)

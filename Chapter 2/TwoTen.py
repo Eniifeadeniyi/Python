a8e7f47e372a5a7653c475892b3e3d49721667c0
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 13:02:50 +0100

    Update TwoTen.py

--- Chapter 2/TwoTen.py
+++ Chapter 2/TwoTen.py
@@ -1,17 +1,17 @@
 #ask user to input 3 integers:
-#convert inputs to integers
+#calculate the sum, average, product, smallest number and largest number.
+#print sum, average, product, smallest number and largest number.
+
 First_Number = int(input("Enter first number: "))
 Second_Number = int(input("Enter second number: "))
 Third_Number = int(input("Enter third number: "))
 
-#calculations:
 sum = First_Number + Second_Number + Third_Number
 average = sum / 3
 product =  First_Number * Second_Number * Third_Number
 smallest = min(First_Number, Second_Number, Third_Number)
 largest = max(First_Number, Second_Number, Third_Number)
 
-#convert back to string to concatenate.
 print("The sum of the numbers is " + str(sum))
 print("The average of the numbers is " + str(average))
 print("The product of the numbers is " + str(product))

00a215bd71b5f4874ceb656791b7d01f20cc24c9
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 13:21:12 +0100

    Update PetChooser.py

--- Snacks for Beginners/PetChooser.py
+++ Snacks for Beginners/PetChooser.py
@@ -1,8 +1,11 @@
-pet = input("Enter your favourite pet: ")
 #Ask for user to type in their favourite pet.
+#Compare user's input with my favourite pet.
+#It should print Woof, Dogs are awesome!, if the user types in dog; and Cool choice, but I love dogs!, if not.
+
+pet = input("Enter your favourite pet: ")
 
 if pet == "dog":
 	print("Woof! Dogs are awesome!")
 else:
 	print("Cool choice, but I love dogs!")
-#It should print Woof, Dogs are awesome!, if the user types in dog; and Cool choice, but I love dogs!, if not.
+

44d5c559357f1f8351d75e4b00edc29609ec0552
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 12:39:59 +0100

    Add files via upload

---
+++ Snacks for Beginners/GreetingChooser.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+#Ask use to type in their name
+
+
+if name == "Alex":
+	print("Hello, friend!")
+
+else:
+	print("Hi, stranger!")
+#Check if the name is Alex or not

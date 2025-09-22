622b70dd4553124b61dee62b83e1c3be9e89e575
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 13:16:39 +0100

    Update EvenOrOdd.py

--- Snacks for Beginners/EvenOrOdd.py
+++ Snacks for Beginners/EvenOrOdd.py
@@ -1,11 +1,11 @@
-number = int(input("Enter a number: "))
 #Ask for user to type in a number.
-#Convert to integer, since input returns as string.
+#Even numbers are divisible by 2, odd numbers are not.
+#It should print EVEN if an even number, and ODD if an odd number
+
+number = int(input("Enter a number: "))
 
 if number % 2 == 0:
 	print("EVEN")
 
 else: 
 	print("ODD")
-#It should print EVEN if an even number, and ODD if an odd number
-#Even numbers are divisible by 2, odd numbers are not.

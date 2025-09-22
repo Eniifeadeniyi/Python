626faf19d3b0de2e945546c25f814e8c79cf7b93
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 23:39:33 +0100

    Delete Chapter 2 directory

--- Chapter 2/TwoSeven.py
+++
@@ -1,9 +0,0 @@
-#If 1024 is a multiple of 4, 1024 divided by 4 will have a remainder equal to 0
- #If 2 is a multiple of 10, 2 divided by 10 will have a remainder equal to 0.
-
-
-if 1024 % 4 == 0:
-	print("1024 is a multiple of 4")     
-
-if 2 % 10 == 0:
-	print("2 is a multiple of 10")      
--- Chapter 2/TwoThree.py
+++
@@ -1,8 +0,0 @@
-#Ask user to input score
-#Compare user's grade with 90
-
-grade = int(input("Enter your grade: "))   
-
-if grade >= 90:                            
-	print("Congratulations! Your grade of " + str(grade) + " earns you an A in this course.")
-

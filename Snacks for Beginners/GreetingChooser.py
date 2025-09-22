581529f50bd248e569c7dd86250e4fda2a3161e7
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 13:20:11 +0100

    Update GreetingChooser.py

--- Snacks for Beginners/GreetingChooser.py
+++ Snacks for Beginners/GreetingChooser.py
@@ -1,10 +1,10 @@
-name = input("Enter your name: ")
-#Ask use to type in their name
+#Ask user to type in their name
+#Check if the name is Alex or not
 
+name = input("Enter your name: ")
 
 if name == "Alex":
 	print("Hello, friend!")
 
 else:
 	print("Hi, stranger!")
-#Check if the name is Alex or not

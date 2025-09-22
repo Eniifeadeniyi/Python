aaf42ad9202c4b3e58aea3202b12826d90eb9a47
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 12:59:58 +0100

    Update TwoSeven.py

--- Chapter 2/TwoSeven.py
+++ Chapter 2/TwoSeven.py
@@ -1,5 +1,9 @@
+#If 1024 is a multiple of 4, 1024 divided by 4 will have a remainder equal to 0
+ #If 2 is a multiple of 10, 2 divided by 10 will have a remainder equal to 0.
+
+
 if 1024 % 4 == 0:
-	print("1024 is a multiple of 4")     #If 1024 is a multiple of 4, 1024 divided by 4 will have a remainder equal to 0
+	print("1024 is a multiple of 4")     
 
 if 2 % 10 == 0:
-	print("2 is a multiple of 10")       #If 2 is a multiple of 10, 2 divided by 10 will have a remainder equal to 0.
+	print("2 is a multiple of 10")      

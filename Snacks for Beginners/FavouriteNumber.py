467a6594c268735bcac8f17128708c50c8f6c51d
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 13:18:50 +0100

    Update FavouriteNumber.py

--- Snacks for Beginners/FavouriteNumber.py
+++ Snacks for Beginners/FavouriteNumber.py
@@ -1,12 +1,13 @@
-favourite_number = 7
 #Make favourite number to be 7.
+#Ask for input from user.
+#Compare user's input with set favourite number.
+
+favourite_number = 7
 
 guess = int(input("Guess a number: "))
-#Ask for input from user.
 
 if guess == favourite_number:
 	print("That's my favourite number!")
 
 else:
 	print("Nice try, guess again!")
-#Compare user's input with set favourite number.

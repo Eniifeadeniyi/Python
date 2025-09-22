0fc1780845be8d3fd7877e841828d159fdc09108
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-22 12:50:39 +0100

    Update TwoFive.py

--- Chapter 2/TwoFive.py
+++ Chapter 2/TwoFive.py
@@ -1,10 +1,16 @@
-radius = 2    #Set radius to be 2
-diameter = radius * 2    #diameter of a circle is twice its radius
-pie = 3.14159             #use given value of pie in question
-circumference = 2 * pie * radius      #formula to find circumference
-area = pie * radius ** 2           #formula to find area
+#Set radius to be 2
+#diameter of a circle is twice its radius
+#use given value of pie in question(3.14159)
+#formula to find circumference
+#formula to find area
+#print area, circumference, and diameter as strings.
 
+radius = 2    
+diameter = radius * 2    
+pie = 3.14159            
+circumference = 2 * pie * radius      
+area = pie * radius ** 2           
 print("The diameter of the circle is " + str(diameter))
 print("The circumference of the circle is " + str(circumference))
 print("The area of the circle is " + str(area))
-#print area, circumference, and diameter as strings.
+

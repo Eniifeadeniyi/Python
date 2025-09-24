b0d0837427a8ce5b63a83cf1c505a949ef2b24bf
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-24 20:33:04 +0100

    Update twice_son_age.py

--- Midnight-Snacks/twice_son_age.py
+++ Midnight-Snacks/twice_son_age.py
@@ -1,3 +1,17 @@
+#Declare a variable, future.
+#Declare a variable, past.
+#Declare a variable, father, and set it to 1.
+#Declare a variable, son, and set it to 1.
+#Ask user to input current age in years.
+#If these conditions are met : 
+	#The father's age should range from 1 to 80, and should always be greater than the son's, assign the entered value to father.
+	#Ask user to input son's current age in years, and assign it to son.
+#If the conditions aren't met, display Unsupported age!
+#Calculate the number of years it will take for the father's age to be twice his son's, and assign it to future.
+#Calculate the number of years ago the father's age was twice his son's, and assign it to past.
+#Print future
+#Print absolute value of past.
+
 father = int(input("Enter your current age(in years): "))
 
 if father > 80 or father < 1:

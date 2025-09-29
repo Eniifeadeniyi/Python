dd763a906d328d9a00fac0c564d3a6d88cd82b17
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-30 00:29:25 +0100

    Update miles_per_gallon.py

--- Chapter-3/miles_per_gallon.py
+++ Chapter-3/miles_per_gallon.py
@@ -4,10 +4,15 @@ gallons = 0
 total_gallons = 0
 
 while miles != -1:
-	total_miles += miles
 	miles = int(input("Enter number of miles driven(or -1 to end): "))
-	total_gallons += gallons
+	if miles == -1 :
+		break
+	total_miles += miles
 	gallons = int (input("Enter gallons of gasoline used: "))
+	journey_average = miles / gallons
+	print("The miles per gallon for this journey was ", journey_average)
+	total_gallons += gallons
 
 average = total_miles / total_gallons
+
 print("The overall average miles/gallon was " + str(average))

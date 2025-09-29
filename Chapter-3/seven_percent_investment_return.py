bad895fd5f86b55c8785bececcc4f3f8957edbee
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-09-29 12:06:46 +0100

    Update seven_percent_investment_return.py

--- Chapter-3/seven_percent_investment_return.py
+++ Chapter-3/seven_percent_investment_return.py
@@ -1,7 +1,7 @@
 principal = 10000
 rate = 0.07
-year = -1
 
-for year in range(0,31):
+for year in range(1,31):
 	interest = principal * (1 + rate) ** year
+
 	print("After year "  + str(year) + ", amount of money is $ " + str(interest))

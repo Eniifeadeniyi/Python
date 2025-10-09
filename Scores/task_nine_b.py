12834e1bed7eefe94af41de81d555dbd11b3b0b7
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-09 09:24:17 +0100

    Add task_eight_b.py

---
+++ Scores/task_nine_b.py
@@ -0,0 +1,13 @@
+count = 0
+sum = 0
+
+while(count < 10):
+	score = int(input("Enter student's score: "))
+	if score < 0 or score > 100:
+		print("Invalid score!")
+	if score >= 0 and score <= 100: 
+		sum += score
+	count += 1
+		
+	
+print("The sum of the valid scores is: " + str(sum))

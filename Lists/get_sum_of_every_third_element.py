c26eb75bedcc2e4255ecc69d5587321145c0161e
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-10-11 07:41:41 +0100

    Update get_sum_of_every_third_element.py

--- Lists/get_sum_of_every_third_element.py
+++ Lists/get_sum_of_every_third_element.py
@@ -1,12 +1,11 @@
 def get_sum_of_every_third_element(*numbers):
-	for number in numbers:
-		count = 0
-		sum = 0
+	count = 0
+	sum = 0
 
-		while count < len(numbers) :
-			count += 1
-			if count % 3 == 0:
-				sum += numbers[count - 1]
+	while count < len(numbers) :
+		count += 1
+		if count % 3 == 0:
+			sum += numbers[count - 1]
 	return sum
 
 print(get_sum_of_every_third_element(1,2,3,4,5))

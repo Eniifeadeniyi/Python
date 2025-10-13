3f7c1d15929015ed15ed1b7461a4fa61e9455c16
Eniifeadeniyi <eniifeoluwaadeniyi16@gmail.com>
2025-10-13 22:19:47 +0100

    Update adding_ing.py

--- Roasted_Corn/adding_ing.py
+++ Roasted_Corn/adding_ing.py
@@ -1,8 +1,6 @@
 def add_ing(word):
 	if len(word) >= 3:
-		if word[-1] == "g":
-			if word[-2] == "n": 
-				if word[-3] == "i": 
+		if word[-1] == "g" and word[-2] == "n" and word[-3] == "i": 
 					return word + "ly"
 
 		return word + "ing"

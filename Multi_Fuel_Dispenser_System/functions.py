e30561f8938165846bc8de8ee7c0270c725802f3
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-20 21:47:52 +0100

    Add functions.py

--- Multi_Fuel_Dispenser_System/functions.py
+++ Multi_Fuel_Dispenser_System/functions.py
@@ -1,9 +1,11 @@
 def petrol_amount(amount, receipts = []):
 	if type(amount) == int or type(amount) == float:
-		if amount >= 1000:
+		if amount >= 1000 and amount <= 50000:
 			number_of_litres = amount / 1000
-		else:
+		elif amount < 1000:
 			return("Amount must be above a liter price!!!")
+		elif amount > 50000:
+			return("You can only purchase a maximum of 50L!!!")
 	else:
 		return("Invalid input!")
 	return amount, number_of_litres	
@@ -39,10 +41,12 @@ def format_petrol_liter(number_of_litres,amount):
 
 def diesel_amount(amount, receipts = []):
 	if type(amount) == int or type(amount) == float:
-		if amount >= 1500:
+		if amount >= 1500 and amount <= 75000:
 			number_of_litres = amount / 1500
-		else:
+		elif amount < 1500:
 			return("Amount must be above a liter price!!!")
+		elif amount > 75000:
+			return("You can only purchase a maximum of 50L!!!")
 	else:
 		return("Invalid input!")
 	return amount,number_of_litres
@@ -70,6 +74,91 @@ def diesel_liter(number_of_litres, receipts = []):
 def format_diesel_liter(number_of_litres,amount):
 	return f"""
 		Product : Diesel       								
+		Amount : #{amount:.2f}  								
+		Liters : {number_of_litres:.2f}L   						
+		Thanks for patronizing Eniife's Station 			
+			Hope to see you again!					
+		"""
+
+
+def kerosene_amount(amount, receipts = []):
+	if type(amount) == int or type(amount) == float:
+		if amount >= 1200 and amount <= 60000:
+			number_of_litres = amount / 1200
+		elif amount < 1200:
+			return("Amount must be above a liter price!!!")
+		elif amount > 60000:
+			return("You can only purchase a maximum of 50L!!!")
+	else:
+		return("Invalid input!")
+	return amount,number_of_litres
+
+def format_kerosene_amount_receipt(amount,number_of_litres):
+	return f"""
+		Product : Kerosene
+		Amount : #{amount:.2f}
+		Liters : {number_of_litres:.2f}
+		Thanks for patronizing Eniife's Station
+			Hope to see you again!
+		"""
+	
+
+def kerosene_liter(number_of_litres, receipts = []):
+	if type(number_of_litres) == int or type(number_of_litres) == float:
+		if number_of_litres >= 1 and number_of_litres <= 50:
+			amount = number_of_litres * 1200
+		else:
+			return("Liters must be between 1-50!!!")
+	else:
+		return("Invalid input!")
+	return amount,number_of_litres	
+
+def format_kerosene_liter(number_of_litres,amount):
+	return f"""
+		Product : Kerosene       								
+		Amount : #{amount:.2f}  								
+		Liters : {number_of_litres:.2f}L   						
+		Thanks for patronizing Eniife's Station 			
+			Hope to see you again!					
+		"""
+
+
+
+def gas_amount(amount, receipts = []):
+	if type(amount) == int or type(amount) == float:
+		if amount >= 1800 and amount <= 90000:
+			number_of_litres = amount / 1800
+		elif amount < 1800:
+			return("Amount must be above a liter price!!!")
+		elif amount > 90000:
+			return("You can only purchase a maximum of 50L!!!")
+	else:
+		return("Invalid input!")
+	return amount,number_of_litres
+
+def format_gas_amount_receipt(amount,number_of_litres):
+	return f"""
+		Product : Gas
+		Amount : #{amount:.2f}
+		Liters : {number_of_litres:.2f}
+		Thanks for patronizing Eniife's Station
+			Hope to see you again!
+		"""
+	
+
+def gas_liter(number_of_litres, receipts = []):
+	if type(number_of_litres) == int or type(number_of_litres) == float:
+		if number_of_litres >= 1 and number_of_litres <= 50:
+			amount = number_of_litres * 1800
+		else:
+			return("Liters must be between 1-50!!!")
+	else:
+		return("Invalid input!")
+	return amount,number_of_litres	
+
+def format_gas_liter(number_of_litres,amount):
+	return f"""
+		Product : Gas       								
 		Amount : #{amount:.2f}  								
 		Liters : {number_of_litres:.2f}L   						
 		Thanks for patronizing Eniife's Station 			
--- Multi_Fuel_Dispenser_System/test_functions.py
+++ Multi_Fuel_Dispenser_System/test_functions.py
@@ -64,3 +64,69 @@ class TestForFuelDispenserSystemFunctions(unittest.TestCase):
 		self.assertEqual(actual,expected)
 
 
+
+	def test_that_kerosene_amount_function_works(self): 
+		actual = kerosene_amount(2400)
+		expected = 2400,2
+		self.assertEqual(actual,expected)
+
+	def test_that_kerosene_amount_function_doesnt_take_string_input(self):
+		actual = kerosene_amount("tgr")
+		expected = "Invalid input!"
+		self.assertEqual(actual,expected)
+
+	def test_that_kerosene_amount_function_doesnt_take_amount_less_than_a_liter(self):
+		actual = kerosene_amount(500)
+		expected = "Amount must be above a liter price!!!"
+		self.assertEqual(actual,expected)
+
+	def test_that_kerosene_liter_function_works(self): 
+		actual = kerosene_liter(2)
+		expected = 2400,2
+		self.assertEqual(actual,expected)
+
+	def test_that_kerosene_liter_function_doesnt_take_string_input(self):
+		actual = kerosene_liter("tgr")
+		expected = "Invalid input!"
+		self.assertEqual(actual,expected)
+
+	def test_that_kerosene_liter_function_only_takes_liter_between_one_and_fifty(self):
+		actual = kerosene_liter(-1)
+		expected = "Liters must be between 1-50!!!"
+		self.assertEqual(actual,expected)
+
+
+	def test_that_gas_amount_function_works(self): 
+		actual = gas_amount(3600)
+		expected = 3600,2
+		self.assertEqual(actual,expected)
+
+	def test_that_gas_amount_function_doesnt_take_string_input(self):
+		actual = gas_amount("tgr")
+		expected = "Invalid input!"
+		self.assertEqual(actual,expected)
+
+	def test_that_gas_amount_function_doesnt_take_amount_less_than_a_liter(self):
+		actual = gas_amount(500)
+		expected = "Amount must be above a liter price!!!"
+		self.assertEqual(actual,expected)
+
+	def test_that_gas_liter_function_works(self): 
+		actual = gas_liter(2)
+		expected = 3600,2
+		self.assertEqual(actual,expected)
+
+	def test_that_gas_liter_function_doesnt_take_string_input(self):
+		actual = gas_liter("tgr")
+		expected = "Invalid input!"
+		self.assertEqual(actual,expected)
+
+	def test_that_gas_liter_function_only_takes_liter_between_one_and_fifty(self):
+		actual = gas_liter(-1)
+		expected = "Liters must be between 1-50!!!"
+		self.assertEqual(actual,expected)
+
+
+
+
+

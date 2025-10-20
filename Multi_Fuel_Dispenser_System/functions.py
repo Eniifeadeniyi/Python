4c8651c7c75b1e62a69e464e46362c9d688266db
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-20 07:52:19 +0100

    Add functions.py

--- Multi_Fuel_Dispenser_System/functions.py
+++ Multi_Fuel_Dispenser_System/functions.py
@@ -1,18 +1,35 @@
 def petrol_amount(amount, receipts = []):
-	if amount >= 1000:
-		number_of_litres = amount / 1000
-		receipt = f"""
-		=========================================================
-		=	Product : Petrol       								=
-		=	Amount : #{amount:.2f}  							=
-		=	Liters : {number_of_litres:.2f}L   					=
-		=	Thanks for patronizing Eniife's Station 			=
-		=		Hope to see you again!							=
-		=========================================================
-		"""
-		receipts.append(receipt)
+	if type(amount) == int or type(amount) == float:
+		if amount >= 1000:
+			number_of_litres = amount / 1000
+			receipt = f"""
+				Product : Petrol       								
+				Amount : #{amount:.2f}  								
+				Liters : {number_of_litres:.2f}L   						
+				Thanks for patronizing Eniife's Station 			
+					Hope to see you again!					
+		
+				"""
+		else:
+			return("Amount must be above a liter price!!!")
 	else:
-		return("Amount must be above a liter price!!!")
-	return receipt
-	
-print(petrol_amount(1000))
+		return("Invalid input!")
+	return receipt	
+
+def petrol_liter(number_of_litres, receipts = []):
+	if type(number_of_litres) == int or type(number_of_litres) == float:
+		if number_of_litres >= 1 and number_of_litres <= 50:
+			amount = number_of_litres * 1000
+			receipt = f"""
+				Product : Petrol       								
+				Amount : #{amount:.2f}  								
+				Liters : {number_of_litres:.2f}L   						
+				Thanks for patronizing Eniife's Station 			
+					Hope to see you again!					
+		
+				"""
+		else:
+			return("Liters must be between 1-50!!!")
+	else:
+		return("Invalid input!")
+	return receipt	
--- Multi_Fuel_Dispenser_System/test_functions.py
+++ Multi_Fuel_Dispenser_System/test_functions.py
@@ -2,16 +2,47 @@ import unittest
 from functions import *
 
 class TestForFuelDispenserSystemFunctions(unittest.TestCase):
-	def test_that_petrol_amount_function_exists(self):
-		receipt = f"""
-		=========================================================
-		=	Product : Petrol       				=
-		=	Amount : #2000.00  				=
-		=	Liters : 2.00L		   			=
-		=	Thanks for patronizing Eniife's Station 	=
-		=		Hope to see you again!			=
-		=========================================================
-		"""
+	def test_that_petrol_amount_function_works(self): 
 		actual = petrol_amount(2000)
-		expected = receipt		
+		expected = f"""
+			Product : Petrol       								
+			Amount : #2000.00 								
+			Liters : 2.00L   						
+			Thanks for patronizing Eniife's Station 			
+				Hope to see you again!			
+		
+			"""
 		self.assertEqual(actual,expected)
+
+	def test_that_petrol_amount_function_doesnt_take_string_input(self):
+		actual = petrol_amount("tgr")
+		expected = "Invalid input!"
+		self.assertEqual(actual,expected)
+
+	def test_that_petrol_amount_function_doesnt_take_amount_less_than_a_liter(self):
+		actual = petrol_amount(500)
+		expected = "Amount must be above a liter price!!!"
+		self.assertEqual(actual,expected)
+
+	def test_that_petrol_liter_function_works(self): 
+		actual = petrol_liter(2)
+		expected = f"""
+			Product : Petrol       								
+			Amount : #2000.00 								
+			Liters : 2.00L   						
+			Thanks for patronizing Eniife's Station 			
+				Hope to see you again!			
+		
+			"""
+		self.assertEqual(actual,expected)
+
+	def test_that_petrol_liter_function_doesnt_take_string_input(self):
+		actual = petrol_liter("tgr")
+		expected = "Invalid input!"
+		self.assertEqual(actual,expected)
+
+	def test_that_petrol_liter_function_only_takes_liter_between_one_and_fifty(self):
+		actual = petrol_liter(-1)
+		expected = "Liters must be between 1-50!!!"
+		self.assertEqual(actual,expected)
+

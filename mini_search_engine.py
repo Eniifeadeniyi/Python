93fd760e475880af80b5f78d87e0255b5f486a0e
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-24 22:09:07 +0100

    mini

--- mini_search_engine.py
+++ mini_search_engine.py
@@ -5,17 +5,25 @@ def count_occurrence(sentence, element):
             count += 1
     return count
 
+def delete(sentence,end_index):
+    return sentence[end_index+1:]
+
 def search_for_element(sentence,element):
-    count = 0
-    start = ()
-    for letter in sentence:
-        for char in element:
-            if letter.lower == char.lower():
-                print("hi")
-                start += (count,)
+    indexes = []
+    index = 0
+    last = 0
+    if sentence.find(element) == -1:
+        return "Not found!"
+    for _ in range(count_occurrence(sentence, element)):
+            print(index)
+            index = sentence.find(element,last)
+            indexes.append(index)
+            last = index + (len(element) - 1)
+            indexes.append(last)
+    return indexes
 
 
 paragraph = input("Enter a paragraph: ")
 key_word = input("Enter a word to search for: ")
 print("Occurence -> " + str(count_occurrence(paragraph, key_word)))
-search_for_element(paragraph, key_word)
+print(search_for_element(paragraph, key_word))

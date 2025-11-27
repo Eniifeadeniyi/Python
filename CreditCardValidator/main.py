7ba0c91d4cf4efae074ec735038263aaa56b45c3
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-27 03:01:26 +0100

    ...

--- CreditCardValidator/main.py
+++ CreditCardValidator/main.py
@@ -5,6 +5,7 @@ card_number = input("Hello, kindly enter card details to verify: ")
 while not card_number.isdigit():
 	print("Invalid input!")
 	card_number = input("Hello, kindly enter card details to verify: ")
+
 if len(card_number) > 12 and len(card_number) < 17:
 	print("Credit Card Type: " + check_card_type(card_number))
 	print("Credit Card Number: " + card_number)
@@ -15,4 +16,5 @@ if len(card_number) > 12 and len(card_number) < 17:
 		print("Credit Card Validity Status: Invalid")
 else:
 	print("Credit Card Number: " + card_number)
+	print("Credit Card Digit Length: " + str(len(card_number)))
 	print("Credit Card Validity Status: Invalid")
